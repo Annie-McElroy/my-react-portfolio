59e898d55e88dedcde014f377f5bed97564a9c84
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2024-01-23 10:52:01 -0600

    Added border styling around project images and description, changed classnames around

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -5,12 +5,12 @@ function ProjectCard(props) {
     const toolsList = props.tools.join(', ');
 
     return (
-        <section>
-            <figure className="ProjectCard">
+        <section className="ProjectCard">
+            <figure className="ProjectCard-figure">
                 <img src={props.imgSrc} alt={props.name} />
                 <figcaption className='ProjectCard-title'>{props.name}</figcaption>
             </figure>
-            <article>
+            <article className='ProjectCard-article'>
                 <p>Description: {props.description}</p>
                 <p>Role: {props.role}</p>
                 <p>Tools: {toolsList} </p>
