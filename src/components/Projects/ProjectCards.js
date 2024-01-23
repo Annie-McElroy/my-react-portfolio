a5c8f118f0aa13e43fd8275441d9b3f54389e824
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2024-01-23 10:47:41 -0600

    Got the description, role, and tool props to display on Projects page

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,12 +2,21 @@ import '../../styles/Project.css';
 
 
 function ProjectCard(props) {
+    const toolsList = props.tools.join(', ');
 
     return (
-        <figure className="ProjectCard">
-            <img src={props.imgSrc} alt={props.name} />
-            <figcaption className='ProjectCard-title'>{props.name}</figcaption>
-        </figure>
+        <section>
+            <figure className="ProjectCard">
+                <img src={props.imgSrc} alt={props.name} />
+                <figcaption className='ProjectCard-title'>{props.name}</figcaption>
+            </figure>
+            <article>
+                <p>Description: {props.description}</p>
+                <p>Role: {props.role}</p>
+                <p>Tools: {toolsList} </p>
+            </article>
+        </section>
+        
     )
 }
 
